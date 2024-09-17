07f3a5df50cf98ad067f9e3308df7c37997e230b
wpsimon09 <simon.potocnak03@gmail.com>
2024-09-17 21:04:15 +0200

    started creating window manager class

--- Includes/WindowManager/WindowManager.cpp
+++ Includes/WindowManager/WindowManager.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "WindowManager.h"
+
+WindowManager::WindowManager()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    m_window = glfwCreateWindow(800, 600, "Hello World!", NULL, NULL);
+}
+
+bool WindowManager::InitWindow()
+{
+}
+
+const float& WindowManager::getWindowWidth() const
+{
+}
+
+const float& WindowManager::getWindowHeight() const
+{
+}
+
+const glm::vec2& WindowManager::getWindowDimensions()
+{
+}
+
+WindowManager::~WindowManager()
+{
+}
--- Includes/WindowManager/WindowManager.h
+++ Includes/WindowManager/WindowManager.h
@@ -5,9 +5,25 @@
 #ifndef WINDOWMANAGER_H
 #define WINDOWMANAGER_H
 
-
+#define GLFW_INCLUDE_VULKAN;
+#include <GLFW/glfw3.h>
+#include "glm/glm.hpp"
 
 class WindowManager {
+public:
+    WindowManager();
+
+    bool InitWindow();
+
+    const float& getWindowWidth() const;
+    const float& getWindowHeight() const;
+    const glm::vec2& getWindowDimensions();
+
+    ~WindowManager();
+private:
+    GLFWwindow* m_window;
+    float m_width, m_height;
+
 
 };
 
