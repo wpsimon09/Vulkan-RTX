6b854a18d415aeb7666b449cfada92af77d34b63
wpsimon09 <simon.potocnak03@gmail.com>
2024-09-23 20:37:28 +0200

    finally added debug utils messenger ´

--- Internal/Vulkan/VulkanCore/VInstance.cpp
+++ Internal/Vulkan/VulkanCore/VInstance.cpp
@@ -10,54 +10,66 @@
 #include "Vulkan/Global/GlobalVariables.hpp"
 
 
-VkBool32 VulkanCore::VulkanInstance::debugCallback(vk::DebugUtilsMessageTypeFlagBitsEXT messageSeverity,
-                                                   vk::DebugUtilsMessageTypeFlagsEXT messageType,
-                                                   const vk::DebugUtilsMessengerCallbackDataEXT *pCallbackData,
-                                                   void *pUserData) {
+VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    vk::DebugUtilsMessageTypeFlagBitsEXT messageSeverity,
+    vk::DebugUtilsMessageTypeFlagsEXT messageType,
+    const vk::DebugUtilsMessengerCallbackDataEXT *pCallbackData,
+    void *pUserData
+    ) {
+
     std::ostringstream message;
 
-    message << vk::to_string( static_cast<vk::DebugUtilsMessageSeverityFlagBitsEXT>( messageSeverity ) ) << ": "
-            << vk::to_string( static_cast<vk::DebugUtilsMessageTypeFlagsEXT>( messageType ) ) << ":\n";
-    message << std::string( "\t" ) << "messageIDName   = <" << pCallbackData->pMessageIdName << ">\n";
-    message << std::string( "\t" ) << "messageIdNumber = " << pCallbackData->messageIdNumber << "\n";
-    message << std::string( "\t" ) << "message         = <" << pCallbackData->pMessage << ">\n";
-    if ( 0 < pCallbackData->queueLabelCount )
-    {
-        message << std::string( "\t" ) << "Queue Labels:\n";
-        for ( uint32_t i = 0; i < pCallbackData->queueLabelCount; i++ )
-        {
-            message << std::string( "\t\t" ) << "LabelName = <" << pCallbackData->pQueueLabels[i].pLabelName << ">\n";
+    message << vk::to_string(static_cast<vk::DebugUtilsMessageSeverityFlagBitsEXT>(messageSeverity)) << ": "
+        << vk::to_string(static_cast<vk::DebugUtilsMessageTypeFlagsEXT>(messageType)) << ":\n";
+    message << std::string("\t") << "messageIDName   = <" << pCallbackData->pMessageIdName << ">\n";
+    message << std::string("\t") << "messageIdNumber = " << pCallbackData->messageIdNumber << "\n";
+    message << std::string("\t") << "message         = <" << pCallbackData->pMessage << ">\n";
+    if (0 < pCallbackData->queueLabelCount) {
+        message << std::string("\t") << "Queue Labels:\n";
+        for (uint32_t i = 0; i < pCallbackData->queueLabelCount; i++) {
+            message << std::string("\t\t") << "LabelName = <" << pCallbackData->pQueueLabels[i].pLabelName << ">\n";
         }
     }
-    if ( 0 < pCallbackData->cmdBufLabelCount )
-    {
-        message << std::string( "\t" ) << "CommandBuffer Labels:\n";
-        for ( uint32_t i = 0; i < pCallbackData->cmdBufLabelCount; i++ )
-        {
-            message << std::string( "\t\t" ) << "LabelName = <" << pCallbackData->pCmdBufLabels[i].pLabelName << ">\n";
+    if (0 < pCallbackData->cmdBufLabelCount) {
+        message << std::string("\t") << "CommandBuffer Labels:\n";
+        for (uint32_t i = 0; i < pCallbackData->cmdBufLabelCount; i++) {
+            message << std::string("\t\t") << "LabelName = <" << pCallbackData->pCmdBufLabels[i].pLabelName << ">\n";
         }
     }
-    if ( 0 < pCallbackData->objectCount )
-    {
-        message << std::string( "\t" ) << "Objects:\n";
-        for ( uint32_t i = 0; i < pCallbackData->objectCount; i++ )
-        {
-            message << std::string( "\t\t" ) << "Object " << i << "\n";
-            message << std::string( "\t\t\t" ) << "objectType   = " << vk::to_string( static_cast<vk::ObjectType>( pCallbackData->pObjects[i].objectType ) ) << "\n";
-            message << std::string( "\t\t\t" ) << "objectHandle = " << pCallbackData->pObjects[i].objectHandle << "\n";
-            if ( pCallbackData->pObjects[i].pObjectName )
-            {
-                message << std::string( "\t\t\t" ) << "ObjectName   = <" << pCallbackData->pObjects[i].pObjectName << ">\n";
+    if (0 < pCallbackData->objectCount) {
+        message << std::string("\t") << "Objects:\n";
+        for (uint32_t i = 0; i < pCallbackData->objectCount; i++) {
+            message << std::string("\t\t") << "Object " << i << "\n";
+            message << std::string("\t\t\t") << "objectType   = " << vk::to_string(
+                static_cast<vk::ObjectType>(pCallbackData->pObjects[i].objectType)) << "\n";
+            message << std::string("\t\t\t") << "objectHandle = " << pCallbackData->pObjects[i].objectHandle << "\n";
+            if (pCallbackData->pObjects[i].pObjectName) {
+                message << std::string("\t\t\t") << "ObjectName   = <" << pCallbackData->pObjects[i].pObjectName <<
+                    ">\n";
             }
         }
+        std::string file = "Error in the file: \t";
+        file += __FILE__;
+        file += "\n";
+
+        Utils::Logger::LogError(file + message.str());
+
+        return false;
     }
-    std::string file = "Error in the file: \t";
-    file += __FILE__;
-    file += "\n";
 
-    Utils::Logger::LogError(file + message.str() );
+}
 
-    return false;
+
+VKAPI_ATTR VkResult VKAPI_CALL vkCreateDebugUtilsMessengerEXT(VkInstance instance,
+                                                              const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo,
+                                                              const VkAllocationCallbacks *pAllocator,
+                                                              VkDebugUtilsMessengerEXT *pMessenger) {
+    return GlobalVariables::pfnVkCreateDebugUtilsMessengerEXT(instance, pCreateInfo, pAllocator, pMessenger);
+}
+
+VKAPI_ATTR void VKAPI_CALL vkDestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT messenger
+                                                         , VkAllocationCallbacks const *pAllocator) {
+    return GlobalVariables::pfnVkDestroyDebugUtilsMessengerEXT(instance, messenger, pAllocator);
 }
 
 VulkanCore::VulkanInstance::VulkanInstance(std::string appname) {
@@ -80,18 +92,22 @@ VulkanCore::VulkanInstance::VulkanInstance(std::string appname) {
     instanceInfo.pApplicationInfo = &applicationInfo;
 
     uint32_t extensionCount = 0;
-    const char** extensions;
+    const char **extensions;
 
     WindowManager::GetRequiredExtensions(extensions, extensionCount);
 
-    std::vector<const char*> extensionVector(extensions, extensions + extensionCount);
-    extensionVector.insert(extensionVector.end(), GlobalVariables::validationLayers.begin(), GlobalVariables::validationLayers.end());
+    std::vector<const char *> extensionVector(extensions, extensions + extensionCount);
 
     if (GlobalState::ValidationLayersEnabled) {
+        extensionVector.insert(extensionVector.end(), GlobalVariables::validationLayers.begin()
+                             , GlobalVariables::validationLayers.end());
         instanceInfo.enabledLayerCount = static_cast<uint32_t>(GlobalVariables::validationLayers.size());
         instanceInfo.ppEnabledLayerNames = GlobalVariables::validationLayers.data();
         instanceInfo.enabledExtensionCount = static_cast<uint32_t>(extensionVector.size());
-        instanceInfo.ppEnabledExtensionNames = GlobalVariables::enabledExtensions.data();
+        instanceInfo.ppEnabledExtensionNames = extensionVector.data();
+    }else {
+        instanceInfo.enabledExtensionCount = static_cast<uint32_t>(extensionVector.size());
+        instanceInfo.ppEnabledExtensionNames = extensionVector.data();
     }
 
     try {
@@ -102,20 +118,34 @@ VulkanCore::VulkanInstance::VulkanInstance(std::string appname) {
         throw std::runtime_error(err.what());
     }
 
+    GlobalVariables::pfnVkCreateDebugUtilsMessengerEXT = reinterpret_cast<PFN_vkCreateDebugUtilsMessengerEXT>(m_instance
+        .getProcAddr("vkCreateDebugUtilsMessengerEXT"));
+    if (!GlobalVariables::pfnVkCreateDebugUtilsMessengerEXT) {
+        throw std::runtime_error("Failed to create debug messenger");
+    }
+    else
+        Utils::Logger::LogSuccess("Created create debug messenger");
+
+    GlobalVariables::pfnVkDestroyDebugUtilsMessengerEXT = reinterpret_cast<PFN_vkDestroyDebugUtilsMessengerEXT>(
+        m_instance.getProcAddr("vkDestroyDebugUtilsMessengerEXT"));
+    if (!GlobalVariables::pfnVkDestroyDebugUtilsMessengerEXT) {
+        throw std::runtime_error("Failed to create destroy debug messenger");
+    }
+    else
+        Utils::Logger::LogSuccess("Created destroy debug messenger");
+
+    vk::DebugUtilsMessageSeverityFlagsEXT severityFlags(vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+        vk::DebugUtilsMessageSeverityFlagBitsEXT::eError);
+    vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags(
+        vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
+        vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation);
+    m_debugMessenger = m_instance.createDebugUtilsMessengerEXT(
+        vk::DebugUtilsMessengerCreateInfoEXT({}, severityFlags, messageTypeFlags, PFN_vkDebugUtilsMessengerCallbackEXT(&debugCallback)));
+
+
 }
 
 VulkanCore::VulkanInstance::~VulkanInstance() {
     Utils::Logger::LogSuccess("Vulkan instance destroyed");
     m_instance.destroy();
-}
-
-void VulkanCore::VulkanInstance::PopulateDebugMessengerCreateInfo(
-    vk::DebugUtilsMessengerCreateInfoEXT &createInfo) {
-
-    vk::DebugUtilsMessageSeverityFlagsEXT severityFlags( vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
-                                                         vk::DebugUtilsMessageSeverityFlagBitsEXT::eError );
-    vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags( vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
-                                                        vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation );
-
-
 }
--- Internal/Vulkan/VulkanCore/VInstance.hpp
+++ Internal/Vulkan/VulkanCore/VInstance.hpp
@@ -16,18 +16,9 @@ namespace VulkanCore
     public:
         explicit VulkanInstance(std::string appname);
 
-        static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
-            vk::DebugUtilsMessageTypeFlagBitsEXT messageSeverity,
-            vk::DebugUtilsMessageTypeFlagsEXT messageType,
-            const vk::DebugUtilsMessengerCallbackDataEXT* pCallbackData,
-            void *pUserData
-            );
 
         ~VulkanInstance();
-
     private:
-        void PopulateDebugMessengerCreateInfo(vk::DebugUtilsMessengerCreateInfoEXT &createInfo);
-
         vk::Instance m_instance;
         vk::DebugUtilsMessengerEXT m_debugMessenger;
     };
