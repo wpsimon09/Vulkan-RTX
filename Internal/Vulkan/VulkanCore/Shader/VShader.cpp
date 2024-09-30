7a535fd1a4909231f9865819b558fc9e8e061aa3
wpsimon09 <simon.potocnak03@gmail.com>
2024-09-30 20:35:11 +0200

    created shader class with implementation

---
+++ Internal/Vulkan/VulkanCore/Shader/VShader.cpp
@@ -0,0 +1,89 @@
+//
+// Created by wpsimon09 on 30/09/24.
+//
+
+#include "VShader.hpp"
+
+#include <fstream>
+
+#include "Includes/Logger/Logger.hpp"
+
+namespace VulkanCore {
+    VShader::VShader(const VulkanCore::VDevice& device ,const std::string& vertexSource, const std::string &fragmentSource, const std::string& computeSource):m_device(device) {
+        m_vertexSource = vertexSource;
+        m_fragmentSource = fragmentSource;
+        if(computeSource != "") {
+            m_computeSource = computeSource;
+        }
+
+        CreateShaderModules();
+
+    }
+
+    void VShader::DeleteExistingShaderModules() {
+        m_device.GetDevice().destroyShaderModule(m_vertexShaderModule);
+        m_device.GetDevice().destroyShaderModule(m_fragmentShaderModule);
+        if(m_computeShaderModule.has_value()) {
+            m_device.GetDevice().destroyShaderModule(m_computeShaderModule.value());
+        }
+        Utils::Logger::LogInfoVerboseOnly("Deleted all unnecessary shader modules");
+    }
+
+    std::vector<char> VShader::ReadSPIRVShader(const std::string &SPIRVShader) {
+        std::ifstream file(SPIRVShader, std::ios::ate | std::ios::binary);
+
+        if(!file.is_open()) {
+            const auto err = "Failed to open SPIRV shader file at path" + SPIRVShader;
+            throw std::runtime_error(err);
+        }
+
+        //create buffer to hold the binary
+        size_t fileSize = (size_t)file.tellg();
+        std::vector<char>buffer(fileSize);
+
+        //go back to the begining and read the file again to get the content
+        file.seekg(0);
+        file.read(buffer.data(),fileSize);
+
+        file.close();
+
+        return buffer;
+    }
+
+    void VShader::CreateShaderModules() {
+
+        auto vertexSPRIV = ReadSPIRVShader(m_vertexSource);
+        auto fragmentSPRIV = ReadSPIRVShader(m_fragmentSource);
+
+        vk::ShaderModuleCreateInfo vertexShaderModuleCreateInfo;
+        vertexShaderModuleCreateInfo.codeSize = vertexSPRIV.size();
+        vertexShaderModuleCreateInfo.pCode = reinterpret_cast<uint32_t*>(vertexSPRIV.data());
+        vertexShaderModuleCreateInfo.pNext= nullptr;
+        m_vertexShaderModule = m_device.GetDevice().createShaderModule(vertexShaderModuleCreateInfo, nullptr);
+        assert(m_vertexShaderModule != VK_NULL_HANDLE);
+        Utils::Logger::LogInfoVerboseOnly("Created Vertex shader module");
+
+
+        vk::ShaderModuleCreateInfo fragmentShaderModuleCreateInfo;
+        fragmentShaderModuleCreateInfo.codeSize = fragmentSPRIV.size();
+        fragmentShaderModuleCreateInfo.pCode = reinterpret_cast<uint32_t*>(fragmentSPRIV.data());
+        fragmentShaderModuleCreateInfo.pNext= nullptr;
+        m_fragmentShaderModule = m_device.GetDevice().createShaderModule(fragmentShaderModuleCreateInfo, nullptr);
+        assert(m_fragmentShaderModule != VK_NULL_HANDLE);
+        Utils::Logger::LogInfoVerboseOnly("Created Fragment shader module");
+
+
+        if(!m_computeSource.has_value()) {
+            auto computeSPRIV = ReadSPIRVShader(m_computeSource.value());
+            vk::ShaderModuleCreateInfo computeShaderModuleCreateInfo;
+            computeShaderModuleCreateInfo.codeSize = computeSPRIV.size();   ;
+            computeShaderModuleCreateInfo.pCode = reinterpret_cast<uint32_t*>(computeSPRIV.data());
+            computeShaderModuleCreateInfo.pNext= nullptr;
+            m_computeShaderModule = m_device.GetDevice().createShaderModule(computeShaderModuleCreateInfo, nullptr);
+            assert(m_computeShaderModule != VK_NULL_HANDLE);
+            Utils::Logger::LogInfoVerboseOnly("Created Compute shader module");
+        }else
+            Utils::Logger::LogInfoVerboseOnly("Compute shader was not specified");
+
+    }
+}// VulkanCore
---
+++ Internal/Vulkan/VulkanCore/Shader/VShader.hpp
@@ -0,0 +1,36 @@
+//
+// Created by wpsimon09 on 30/09/24.
+//
+
+#ifndef VSHADER_HPP
+#define VSHADER_HPP
+#include <string>
+#include <vulkan/vulkan.hpp>
+
+#include "Vulkan/VulkanCore/Device/VDevice.hpp"
+
+
+namespace VulkanCore {
+    class VShader {
+        public:
+            VShader(const VulkanCore::VDevice& device ,const std::string& vertexSource, const std::string &fragmentSource, const std::string& computeSource = "");
+
+            void DeleteExistingShaderModules();
+            std::vector<char> ReadSPIRVShader(const std::string& SPIRVShader);
+        private:
+            void CreateShaderModules();
+        private:
+            const VulkanCore::VDevice& m_device;
+
+            vk::ShaderModule m_fragmentShaderModule;
+            vk::ShaderModule m_vertexShaderModule;
+            std::optional<vk::ShaderModule> m_computeShaderModule;
+
+            std::string m_vertexSource;
+            std::string m_fragmentSource;
+            std::optional<std::string> m_computeSource;
+    };
+
+} // VulkanCore
+
+#endif //VSHADER_HPP
