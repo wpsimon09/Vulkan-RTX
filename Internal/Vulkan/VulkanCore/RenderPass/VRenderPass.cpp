e3d6d8a72d96cdd7fd1c46c1491e6e58eb46638e
wpsimon09 <simon.potocnak03@gmail.com>
2024-10-06 18:40:35 +0200

    started to write render pass

---
+++ Internal/Vulkan/VulkanCore/RenderPass/VRenderPass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wpsimon09 on 06/10/24.
+//
+
+#include "VRenderPass.hpp"
---
+++ Internal/Vulkan/VulkanCore/RenderPass/VRenderPass.hpp
@@ -0,0 +1,28 @@
+//
+// Created by wpsimon09 on 06/10/24.
+//
+
+#ifndef VRENDERPASS_HPP
+#define VRENDERPASS_HPP
+
+#include <vulkan/vulkan.hpp>
+
+namespace VulkanCore
+{
+    class VDevice;
+    class VSwapChain;
+
+    class VRenderPass {
+    public:
+        VRenderPass(const VulkanCore::VDevice& device, const VulkanCore::VSwapChain& swapChain);
+    private:
+        const VulkanCore::VDevice& m_device;
+        const VulkanCore::VSwapChain& m_swapChain;
+
+        vk::AttachmentDescription m_colourAttachmentDescription;
+    };
+}
+
+
+
+#endif //VRENDERPASS_HPP
