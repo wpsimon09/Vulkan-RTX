09d99d263b8a2ba83bf3e3f020c28608e57dd7ba
wpsimon09 <simon.potocnak03@gmail.com>
2024-10-29 20:22:38 +0100

    added classes for descriptor pool and descriptor builder

---
+++ Internal/Vulkan/VulkanCore/Descriptors/VDescriptorPool.cpp
@@ -0,0 +1,8 @@
+//
+// Created by wpsimon09 on 29/10/24.
+//
+
+#include "VDescriptorPool.hpp"
+
+namespace VulkanCore {
+} // VulkanCore
---
+++ Internal/Vulkan/VulkanCore/Descriptors/VDescriptorPool.hpp
@@ -0,0 +1,23 @@
+//
+// Created by wpsimon09 on 29/10/24.
+//
+
+#ifndef VDESCRIPTORPOOL_HPP
+#define VDESCRIPTORPOOL_HPP
+#include "Vulkan/VulkanCore/VObject.hpp"
+
+namespace VulkanCore {
+
+
+class VDescriptorPoolBuilder {
+
+};
+
+class VDescriptorPool: public VObject {
+
+};
+
+
+} // VulkanCore
+
+#endif //VDESCRIPTORPOOL_HPP
