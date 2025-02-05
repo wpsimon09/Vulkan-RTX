349bb720b080dbfc932a28cf0e9ef9fdfbfe8426
wpsimon09 <simon.potocnak03@gmail.com>
2025-02-05 11:04:37 +0100

    fixed bug where lights were overwriting each others

--- Internal/Application/AssetsManger/AssetsManager.cpp
+++ Internal/Application/AssetsManger/AssetsManager.cpp
@@ -162,11 +162,16 @@ namespace ApplicationCore
 
     std::shared_ptr<StaticMesh> AssetsManager::GetEditorBilboardMesh(EEditorIcon icon)
     {
-        auto mesh = GetDefaultMesh(EMeshGeometryType::PostProcessQuad);
-        mesh->SetMaterial(m_editorIconsMaterials[icon]);
+
+        auto data = m_meshDataManager.AddMeshData(MeshData::planeVertices, MeshData::planeIndices);
+
+        auto mesh = std::make_shared<StaticMesh>(data, m_editorIconsMaterials[icon]);
+
         mesh->SetName( IconToString(icon)+"##" + VulkanUtils::random_string(4));
 
-        return mesh;
+        m_meshDataManager.UpdateGPU(nullptr);
+
+        return std::move(mesh);
     }
 
     void AssetsManager::AddMesh(std::string meshName, std::shared_ptr<StaticMesh> mesh)
