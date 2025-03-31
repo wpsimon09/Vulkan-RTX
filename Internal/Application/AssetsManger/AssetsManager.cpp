aca43dabe0d8c9ef0a55d4175995d45a787f13d8
wpsimon09 <simon.potocnak03@gmail.com>
2025-03-31 07:43:13 +0200

    removed the need for sync

--- Internal/Application/AssetsManger/AssetsManager.cpp
+++ Internal/Application/AssetsManger/AssetsManager.cpp
@@ -33,16 +33,16 @@
 namespace ApplicationCore
 {
     AssetsManager::AssetsManager(const VulkanCore::VDevice& device
-        ,VulkanCore::MeshDatatManager& meshDataManager
-        ,ApplicationCore::EffectsLibrary& effectsLibrary):
-        m_device(device), m_meshDataManager(meshDataManager),m_transferOpsManager(device.GetTransferOpsManager()), m_materials(), m_effectsLibrary(effectsLibrary)
+                                 , VulkanCore::MeshDatatManager& meshDataManager
+                                 , ApplicationCore::EffectsLibrary& effectsLibrary):
+        m_device(device), m_meshDataManager(meshDataManager), m_transferOpsManager(device.GetTransferOpsManager()),
+        m_materials(), m_effectsLibrary(effectsLibrary)
     {
-       CreateDefaultAssets();
+        CreateDefaultAssets();
     }
 
     void AssetsManager::DeleteAll()
     {
-
         m_dummyImage->Destroy();
         for (auto& texture : m_textures)
         {
@@ -57,10 +57,10 @@ namespace ApplicationCore
             if (hdr.second)
                 hdr.second->Destroy();
         }
-        for (auto& textureAsset : m_textures2){
+        for (auto& textureAsset : m_textures2)
+        {
             textureAsset.second->Destroy();
         }
-
     }
 
     std::shared_ptr<StaticMesh> AssetsManager::GetDefaultMesh(EMeshGeometryType geometryType)
@@ -69,7 +69,6 @@ namespace ApplicationCore
         // if they are loaded return the loaded result
         if (result != m_preloadedMeshes.end())
         {
-
             return std::make_shared<StaticMesh>(result->second, m_dummyMaterial);
         }
         // load them otherwise
@@ -110,7 +109,7 @@ namespace ApplicationCore
             }
         case PostProcessQuad:
             {
-                data = m_meshDataManager.AddMeshData(MeshData::fullscreenQuadVertices,MeshData::fullscreenQuadIndices);
+                data = m_meshDataManager.AddMeshData(MeshData::fullscreenQuadVertices, MeshData::fullscreenQuadIndices);
                 break;
             }
         case Arrow:
@@ -139,23 +138,27 @@ namespace ApplicationCore
         return std::make_shared<StaticMesh>(data, m_dummyMaterial);
     }
 
-    void AssetsManager::GetTexture(std::shared_ptr<ApplicationCore::VTextureAsset>& texture, const std::string& path, bool saveToDisk)
+    void AssetsManager::GetTexture(std::shared_ptr<ApplicationCore::VTextureAsset>& texture, const std::string& path,
+                                   bool saveToDisk)
     {
-        
-        if (!m_textures2.contains(path)){
-            m_textures2[path] = std::make_shared<ApplicationCore::VTextureAsset> (m_device, m_dummyImage, ETextureAssetType::Texture, path);
+        if (!m_textures2.contains(path))
+        {
+            m_textures2[path] = std::make_shared<ApplicationCore::VTextureAsset>(
+                m_device, m_dummyImage, ETextureAssetType::Texture, path);
             m_texturesToSync.emplace_back(m_textures2[path]);
         }
 
         texture = m_textures2[path];
     }
 
-    void AssetsManager::GetTexture(std::shared_ptr<ApplicationCore::VTextureAsset>& texture, const std::string& textureID,
+    void AssetsManager::GetTexture(std::shared_ptr<ApplicationCore::VTextureAsset>& texture,
+                                   const std::string& textureID,
                                    TextureBufferInfo& data, bool saveToDisk)
     {
-
-        if (!m_textures2.contains(data.textureID)){
-            m_textures2[data.textureID] = std::make_shared<ApplicationCore::VTextureAsset> (m_device, m_dummyImage, ETextureAssetType::Texture, data);
+        if (!m_textures2.contains(data.textureID))
+        {
+            m_textures2[data.textureID] = std::make_shared<ApplicationCore::VTextureAsset>(
+                m_device, m_dummyImage, ETextureAssetType::Texture, data);
             m_texturesToSync.emplace_back(m_textures2[data.textureID]);
         }
 
@@ -163,11 +166,12 @@ namespace ApplicationCore
     }
 
     void AssetsManager::GetHDRTexture(std::shared_ptr<ApplicationCore::VTextureAsset>& texture, const std::string& path,
-        bool saveToDisk)
+                                      bool saveToDisk)
     {
         if (!m_HDRTextures.contains(path))
         {
-            m_HDRTextures[path] = std::make_shared<ApplicationCore::VTextureAsset> (m_device, m_dummyImage, ETextureAssetType::HDRTexture, path);
+            m_HDRTextures[path] = std::make_shared<ApplicationCore::VTextureAsset>(
+                m_device, m_dummyImage, ETextureAssetType::HDRTexture, path);
             m_texturesToSync.emplace_back(m_HDRTextures[path]);
         }
         texture = m_HDRTextures[path];
@@ -180,16 +184,18 @@ namespace ApplicationCore
         //materials.reserve(m_materials.size() +  m_editorIconsMaterials.size() );
 
 
-        for (auto &material : m_materials)
+        for (auto& material : m_materials)
         {
-            if(material->IsSavable()){
+            if (material->IsSavable())
+            {
                 materials.emplace_back(material);
             }
         }
 
-        for (auto &material : m_editorIconsMaterials)
+        for (auto& material : m_editorIconsMaterials)
         {
-            if(material.second->IsSavable()){
+            if (material.second->IsSavable())
+            {
                 materials.emplace_back(material.second);
             }
         }
@@ -206,11 +212,10 @@ namespace ApplicationCore
 
     std::shared_ptr<StaticMesh> AssetsManager::GetEditorBilboardMesh(EEditorIcon icon)
     {
-
         auto mesh = GetDefaultMesh(Plane);
         mesh->SetMaterial(m_editorIconsMaterials[icon]);
 
-        mesh->SetName( IconToString(icon)+"##" + VulkanUtils::random_string(4));
+        mesh->SetName(IconToString(icon) + "##" + VulkanUtils::random_string(4));
 
         return std::move(mesh);
     }
@@ -264,7 +269,7 @@ namespace ApplicationCore
             if (skyBoxMat->GetMaterialName() == name)
             {
                 skyBoxMat->GetHDRTexture()->Destroy();
-                m_skyBoxMaterials.erase(m_skyBoxMaterials.begin()+i);
+                m_skyBoxMaterials.erase(m_skyBoxMaterials.begin() + i);
                 return;
             }
             i++;
@@ -274,17 +279,21 @@ namespace ApplicationCore
     bool AssetsManager::Sync()
     {
         int i = 0;
-        for(auto& tex : m_texturesToSync){
-
-            if (!m_texturesToSync.empty())
+        if (!m_texturesToSync.empty())
+        {
+            for (auto& tex : m_texturesToSync)
             {
-                if (tex->Sync())
+                if (tex)
                 {
-                    m_texturesToSync.erase(m_texturesToSync.begin()+ (i-1));
-                  //  m_texturesToSync.shrink_to_fit();
+                    if (tex->Sync())
+                    {
+                        m_texturesToSync.erase(m_texturesToSync.begin() + (i));
+                        //m_texturesToSync.shrink_to_fit();
+                    }
                 }
                 i++;
             }
+            return false;
         }
         return false;
     }
@@ -330,27 +339,28 @@ namespace ApplicationCore
         //=======================================
         // LTC TEXTURES
         //=======================================
-        auto ltcTexture = std::make_shared<VulkanCore::VImage2>(m_device,  MathUtils::LTC_ImageData);
+        auto ltcTexture = std::make_shared<VulkanCore::VImage2>(m_device, MathUtils::LTC_ImageData);
         m_transferOpsManager.DestroyBuffer(ltcTexture->GetImageStagingvBuffer(), true);
         MathUtils::LUT.LTC = std::make_shared<VTextureAsset>(m_device, std::move(ltcTexture));
 
         ltcTexture = std::make_shared<VulkanCore::VImage2>(m_device, MathUtils::LTCInverse_ImageData);
         m_transferOpsManager.DestroyBuffer(ltcTexture->GetImageStagingvBuffer(), true);
-        MathUtils::LUT.LTCInverse =  std::make_shared<VTextureAsset>(m_device, std::move(ltcTexture));
+        MathUtils::LUT.LTCInverse = std::make_shared<VTextureAsset>(m_device, std::move(ltcTexture));
 
 
-       Sync();
+        Sync();
     }
-    
+
     std::vector<TextureBufferView> AssetsManager::ReadBackAllTextures(std::vector<std::byte>& data)
     {
-    //prepare the data
+        //prepare the data
         std::vector<TextureBufferView> views;
         views.reserve(m_textures.size());
-        
-        for(auto& texture: m_textures2){
-            
-            if(texture.second->IsSavable()){
+
+        for (auto& texture : m_textures2)
+        {
+            if (texture.second->IsSavable())
+            {
                 TextureBufferView textureView;
                 auto& textureInfo = texture.second->GetHandle()->GetImageInfo();
                 textureView.widht = textureInfo.width;
@@ -363,7 +373,7 @@ namespace ApplicationCore
         }
 
 
-        return  std::move(views);
+        return std::move(views);
     }
 }
 
