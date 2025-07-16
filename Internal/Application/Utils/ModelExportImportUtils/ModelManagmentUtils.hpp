040ed0201e093738d9981657d22dc67206a27169
wpsimon09 <simon.potocnak03@gmail.com>
2025-07-16 19:35:38 +0200

    used MikkTspace to calculate the tangent vectors

--- Internal/Application/Utils/ModelExportImportUtils/ModelManagmentUtils.hpp
+++ Internal/Application/Utils/ModelExportImportUtils/ModelManagmentUtils.hpp
@@ -5,21 +5,25 @@
 #ifndef MODELMANAGMENTUTILS_HPP
 #define MODELMANAGMENTUTILS_HPP
 
+#include "Application/VertexArray/VertexArray.hpp"
 #include "Vulkan/Global/VulkanStructs.hpp"
 #include "filesystem"
 #include "fastgltf/math.hpp"
+#include <cstdint>
 struct TextureBufferInfo;
 namespace ApplicationCore {
 VulkanStructs::VImageData<> LoadImage(const std::string& path, bool saveToDisk = false);
 VulkanStructs::VImageData<> LoadImage(const TextureBufferInfo& data, const std::string& textureID, bool saveToDisk = false);
-VulkanStructs::VImageData<float> LoadHDRImage(const std::string& path, bool saveToDisk = false);
-std::vector<VulkanStructs::VImageData<float>> LoadTextureArray(const std::string& folderName,  bool saveToDisk = false);
+VulkanStructs::VImageData<float>              LoadHDRImage(const std::string& path, bool saveToDisk = false);
+std::vector<VulkanStructs::VImageData<float>> LoadTextureArray(const std::string& folderName, bool saveToDisk = false);
 
 void SaveImageAsPNG(int width, int height, int channels, const std::string& path, const std::vector<std::byte>& data);
 void SaveImageAsHDR(int width, int height, int channels, const std::string& path, float* data);
 
 bool      CheckIfImageExistsInFolader(const std::filesystem::path& folder, const std::filesystem::path& image);
 glm::mat4 FastGLTFToGLMMat4(fastgltf::math::fmat4x4* matrix);
+
+bool GeneratTangents(std::vector<ApplicationCore::Vertex>& vertices, std::vector<uint32_t>& indices);
 
 }  // namespace ApplicationCore
 
